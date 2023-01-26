90b2c553aef92881834396106aa60f3962b9ab17
Rishabh Indoria <113393377+rishabhindoria25@users.noreply.github.com>
2023-01-25 19:23:03 -0500

    adding counter

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from PIL import Image
 
 with open("style.css") as f:
     st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
-
+st.markdown('![Visitor count](https://shields-io-visitor-counter.herokuapp.com/badge?page=https://share.streamlit.io/https://rishabhindoria25-resume-streamlit-app-rxhkpg.streamlit.app&label=VisitorsCount&labelColor=000000&logo=GitHub&logoColor=FFFFFF&color=1D70B8&style=for-the-badge)')
 #####################
 # Header 
 st.write('''
