9162eb5e52fb8d81c600ec9e7e6bd8e24f24be3b
Rishabh Indoria <113393377+rishabhindoria25@users.noreply.github.com>
2023-10-03 18:57:22 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ st.set_page_config(page_title="Rishabh Indoria", page_icon=image,layout="centere
 st.write("# **<span style='color:#000000'>Rishabh Indoria</span>** ~~THE IT CROWD~~", unsafe_allow_html=True)
 image = Image.open('dp.png')
 
-col1, col2, col3= st.columns(2)
+col1, col2, col3= st.columns(3)
 with col1:
     st.image(image, width=150)
 with col3:
