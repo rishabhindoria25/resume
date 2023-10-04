3482ee637e0eeb6e6e507955af7e5d803cfd6a27
Rishabh Indoria <113393377+rishabhindoria25@users.noreply.github.com>
2023-10-04 00:23:24 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -364,7 +364,7 @@ st.info('''
 st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
 st.markdown("""
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #021014;">
-  <a style="color: #16A2CB" class="navbar-brand" href="/">>Rishabh Indoria<span class="sr-only">(current)</span></a>
+  <a style="color: #16A2CB" class="navbar-brand" href="/">Rishabh Indoria<span class="sr-only">(current)</span></a>
   <!-- <a style="color: #16A2CB" class="navbar-brand" target="_blank">Rishabh Indoria</a> -->
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
