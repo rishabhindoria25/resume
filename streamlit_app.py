0610ca01cd4d9060fb3e66416461abeb4c7357fa
Rishabh Indoria <113393377+rishabhindoria25@users.noreply.github.com>
2023-10-04 00:59:04 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -580,7 +580,8 @@ for index, (platform, link) in enumerate(SOCIAL_MEDIA.items()):
     with cols[index].container():
         st.markdown(f"<p style='text-align:center'><a href='{link}'>{platform}</a></p>", unsafe_allow_html=True)
 with cols[3]:
-  st.write(Email)
+  # st.write(Email)
+  st.markdown(f"<a href='mailto:{Email}'>Email</a>", unsafe_allow_html=True)
 
 st.markdown("""---""")
 # Summary
