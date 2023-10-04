a4a51aae59539628530489c952186438cd5557cb
Rishabh Indoria <113393377+rishabhindoria25@users.noreply.github.com>
2023-10-03 23:57:39 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 from streamlit_timeline import timeline
 from PIL import Image
+from htbuilder import HtmlElement, div, ul, li, br, hr, a, p, img, styles, classes, fonts
+from htbuilder.units import percent, px
+from htbuilder.funcs import rgba, rgb
+
 image = Image.open("icon.jpg")
 profile_pic = Image.open('dp.png')
 resume_file = "RishabhIndoria_Resume.pdf"
@@ -251,3 +255,59 @@ hide_streamlit_style = """
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+style = """
+<style>
+  #MainMenu {visibility: hidden;}
+  footer {visibility: hidden;}
+ .stApp { bottom: 105px; }
+</style>
+"""
+
+style_div = styles(
+    position="fixed",
+    left=0,
+    bottom=0,
+    margin=px(0, 0, 0, 0),
+    width=percent(100),
+    color="black",
+    text_align="center",
+    height="auto",
+    opacity=1
+)
+
+style_hr = styles(
+    display="block",
+    margin=px(8, 8, "auto", "auto"),
+    border_style="inset",
+    border_width=px(2)
+)
+
+body = p()
+foot = div(
+    style=style_div
+)(
+    hr(
+        style=style_hr
+    ),
+    body
+)
+
+st.markdown(style, unsafe_allow_html=True)
+
+myargs = [
+    "Made in ",
+    img('https://avatars3.githubusercontent.com/u/45109972?s=400&v=4', width=px(25), height=px(25)),
+    " with ❤️ by ",
+    a(_href="https://twitter.com/ChristianKlose3", _target="_blank")("@ChristianKlose3"),
+    br(),
+    a(_href="https://buymeacoffee.com/chrischross", img('https://i.imgur.com/thJhzOO.png')),
+]
+
+for arg in myargs:
+    if isinstance(arg, str):
+        body(arg)
+    elif isinstance(arg, HtmlElement):
+        body(arg)
+
+st.markdown(str(foot), unsafe_allow_html=True)
