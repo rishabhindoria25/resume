f49b319e6fef2c93532db2e55ac6c4e7858b301e
Rishabh Indoria <113393377+rishabhindoria25@users.noreply.github.com>
2023-10-04 00:54:27 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -574,14 +574,17 @@ st.download_button(
 
 # st.write("Email", Email)
 # st.write("Email",Email)
+st.markdown("""---""")
 cols = st.columns(len(SOCIAL_MEDIA)+1)
 for index, (platform, link) in enumerate(SOCIAL_MEDIA.items()):
     with cols[index].container():
         st.markdown(f"<p style='text-align:center'><a href='{link}'>{platform}</a></p>", unsafe_allow_html=True)
 with cols[3]:
   st.write(Email)
+  
 # Summary
 st.markdown('## Summary', unsafe_allow_html=True)
+st.markdown("""---""")
 st.info('''
 &ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;**HAVE YOU TRIED TURNING IT OFF AND ON AGAIN?**
 \nI am a highly accomplished data scientist with a strong focus on achieving measurable results. I possess extensive experience working within cross-functional teams to achieve organizational objectives. Being highly motivated to innovate existing business processes, I achieve optimal results in an efficient and cost-effective manner, utilizing my in-depth domain expertise. I have consistently demonstrated exceptional diligence and commitment to excellence in both my coursework and various work. I am eager to continue this level of performance in future projects, utilizing diverse models, frameworks, and data.
