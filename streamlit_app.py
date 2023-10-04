bf8a626dfdd265ffa85d1e33b7195b18f360a4c8
Rishabh Indoria <113393377+rishabhindoria25@users.noreply.github.com>
2023-10-03 20:05:34 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -233,15 +233,15 @@ st.markdown('''
 # cols = st.columns(len(SOCIAL_MEDIA))
 # for index, (platform, link) in enumerate(SOCIAL_MEDIA.items()):
 #     cols[index].write(f"[{platform}]({link})")
+st.footer()
+# with st.footer("Copyright © 2023 Example"):
 
-with st.footer("Copyright © 2023 Example"):
+#         # Create columns for each social media platform
+#         cols = st.columns(len(SOCIAL_MEDIA))
 
-        # Create columns for each social media platform
-        cols = st.columns(len(SOCIAL_MEDIA))
-
-        # Iterate over the social media platforms and add links to the footer
-        for index, (platform, link) in enumerate(SOCIAL_MEDIA.items()):
-            cols[index].write(f"[{platform}]({link})")
+#         # Iterate over the social media platforms and add links to the footer
+#         for index, (platform, link) in enumerate(SOCIAL_MEDIA.items()):
+#             cols[index].write(f"[{platform}]({link})")
 with open("style.css") as f:
     st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
 # st.markdown('![Visitor count](https://shields-io-visitor-counter.herokuapp.com/badge?page=https://share.streamlit.io/https://rishabhindoria25-resume-streamlit-app-rxhkpg.streamlit.app&label=VisitorsCount&labelColor=000000&logo=GitHub&logoColor=FFFFFF&color=1D70B8&style=for-the-badge)')
