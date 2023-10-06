a8d03281353ace2f65d40c36d059f98b111c2b85
Rishabh Indoria <113393377+rishabhindoria25@users.noreply.github.com>
2023-10-06 01:38:28 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ st.write("# **<span style='color:#000000'>Rishabh Indoria</span>** ~~THE IT CROW
 # col1, col2, col3 = st.columns(3)
 # with col2:
 #     st.image(profile_pic)
-col1, col2, col3 = st.columns([0.01, 1.5, 3.5])
+col1, col2, col3 = st.columns([0.01, 2, 5])
 col2.image(profile_pic, use_column_width=True)
 # st.write("""
 #     <div style="display: flex; justify-content: center;">
@@ -84,7 +84,7 @@ st.markdown("""---""")
 st.markdown('## Summary', unsafe_allow_html=True)
 st.markdown("""---""")
 st.info('''
-&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;**HAVE YOU TRIED TURNING IT OFF AND ON AGAIN?**
+&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;**HAVE YOU TRIED TURNING IT OFF AND ON AGAIN?**
 \nI am a highly accomplished data scientist with a strong focus on achieving measurable results. I possess extensive experience working within cross-functional teams to achieve organizational objectives. Being highly motivated to innovate existing business processes, I achieve optimal results in an efficient and cost-effective manner utilizing my in-depth domain expertise. I have consistently demonstrated exceptional diligence and commitment to excellence in both my coursework and professional work. I am eager to continue this level of performance in future projects, utilizing diverse models, frameworks, and data.
 ''')
 
